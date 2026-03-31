d3f064042e2f5807653abd317825c46cffa7b9cb
Wojciech Turek <wojciechturek9@gmail.com>
2026-03-31 19:13:37 +0200

    test: make usecases pass tests

---
+++ test/src/on_boarding/domain/usecases/check_if_user_is_first_timer_test.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:education_app/src/on_boarding/domain/usecases/check_if_user_is_first_timer.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import '../on_boarding_repo.mock.dart';
+
+void main() {
+  late MockOnBoardingRepo repo;
+  late CheckIfUserIsFirstTimer usecase;
+
+  setUp(() {
+    repo = MockOnBoardingRepo();
+    usecase = CheckIfUserIsFirstTimer(repo);
+  });
+
+  test(
+    'should get a response from the [MockOnBoardingRepo]',
+    () async {
+      when(() => repo.checkIfUserIsFirstTimer()).thenAnswer(
+        (_) async => const Right(true),
+      );
+
+      final result = await usecase();
+
+      expect(
+        result,
+        equals(
+          const Right<dynamic, bool>(true),
+        ),
+      );
+      verify(() => repo.checkIfUserIsFirstTimer()).called(1);
+      verifyNoMoreInteractions(repo);
+    },
+  );
+}
