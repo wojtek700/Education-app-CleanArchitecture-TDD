a616e5d7fc56ef16bd22365d2f5b52bd8e1db39c
Wojciech Turek <wojciechturek9@gmail.com>
2026-04-03 01:37:29 +0200

    test: make OnBoardingRepoImpl pass tests

--- lib/src/on_boarding/data/repos/on_boarding_repo_impl.dart
+++ lib/src/on_boarding/data/repos/on_boarding_repo_impl.dart
@@ -1,3 +1,6 @@
+import 'package:dartz/dartz.dart';
+import 'package:education_app/core/errors/exceptions.dart';
+import 'package:education_app/core/errors/failures.dart';
 import 'package:education_app/core/utils/typedefs.dart';
 import 'package:education_app/src/on_boarding/data/datasources/on_boarding_local_data_source.dart';
 import 'package:education_app/src/on_boarding/domain/repos/on_boarding_repo.dart';
@@ -8,9 +11,15 @@ class OnBoardingRepoImpl implements OnBoardingRepo {
   final OnBoardingLocalDataSource _localDataSource;
 
   @override
-  ResultFuture<void> cacheFirstTimer() {
-    // TODO: implement cacheFirstTimer
-    throw UnimplementedError();
+  ResultFuture<void> cacheFirstTimer() async {
+    try {
+      await _localDataSource.cacheFirstTimer();
+      return const Right(null);
+    } on CacheException catch (e) {
+      return Left(
+        CacheFailure(message: e.message, statusCode: e.statusCode),
+      );
+    }
   }
 
   @override
