a50ab9a193d37808f7144b664a2b85143566b489
Wojciech Turek <wojciechturek9@gmail.com>
2026-04-08 21:31:17 +0200

    feat: add OnBoardingLocalDataSourceImpl

--- lib/src/on_boarding/data/datasources/on_boarding_local_data_source.dart
+++ lib/src/on_boarding/data/datasources/on_boarding_local_data_source.dart
@@ -1,6 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 abstract class OnBoardingLocalDataSource {
   const OnBoardingLocalDataSource();
 
   Future<void> cacheFirstTimer();
   Future<bool> checkIfUserIsFirstTimer();
+}
+
+class OnBoardingLocalDataSourceImpl implements OnBoardingLocalDataSource {
+  const OnBoardingLocalDataSourceImpl(this._prefs);
+
+  final SharedPreferences _prefs;
+
+  @override
+  Future<void> cacheFirstTimer() {
+    // TODO: implement cacheFirstTimer
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> checkIfUserIsFirstTimer() {
+    // TODO: implement checkIfUserIsFirstTimer
+    throw UnimplementedError();
+  }
 }
