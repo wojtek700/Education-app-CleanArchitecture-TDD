2b17a0171813b027782ee03da3c671eb7f153a1f
Wojciech Turek <wojciechturek9@gmail.com>
2026-05-13 20:28:41 +0200

    feat: add OnBoarding UI

---
+++ lib/core/common/views/loading_view.dart
@@ -0,0 +1,20 @@
+import 'package:education_app/core/extensions/context_extensions.dart';
+import 'package:flutter/material.dart';
+
+class LoadingView extends StatelessWidget {
+  const LoadingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(
+            context.theme.colorScheme.secondary,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- test/src/on_boarding/data/repos/on_boarding_repo_impl_test.dart
+++ test/src/on_boarding/data/repos/on_boarding_repo_impl_test.dart
@@ -14,25 +14,20 @@ void main() {
   late OnBoardingLocalDataSource localDataSource;
   late OnBoardingRepoImpl repoImpl;
 
-  setUp(
-    () {
-      localDataSource = MockOnBoardingLocalDataSrc();
-      repoImpl = OnBoardingRepoImpl(localDataSource);
-    },
-  );
-  test(
-    'should be a subclass of [OnBoardingRepo]',
-    () {
-      expect(repoImpl, isA<OnBoardingRepo>());
-    },
-  );
+  setUp(() {
+    localDataSource = MockOnBoardingLocalDataSrc();
+    repoImpl = OnBoardingRepoImpl(localDataSource);
+  });
+
+  test('should be a subclass of [OnBoardingRepo]', () {
+    expect(repoImpl, isA<OnBoardingRepo>());
+  });
+
   group('cacheFirstTimer', () {
     test(
       'should complete successfully when call to local source is successful',
       () async {
-        when(
-          () => localDataSource.cacheFirstTimer(),
-        ).thenAnswer(
+        when(() => localDataSource.cacheFirstTimer()).thenAnswer(
           (_) async => Future.value(),
         );
 
@@ -44,18 +39,18 @@ void main() {
             const Right<dynamic, void>(null),
           ),
         );
-        verify(
-          () => localDataSource.cacheFirstTimer(),
-        ).called(1);
+        verify(() => localDataSource.cacheFirstTimer());
         verifyNoMoreInteractions(localDataSource);
       },
     );
+
     test(
-      'should return [CacheFailure] when call to local source is unsuccessful',
+      'should return [CacheFailure] when call to local source is '
+      'unsuccessful',
       () async {
-        when(
-          () => localDataSource.cacheFirstTimer(),
-        ).thenThrow(const CacheException(message: 'Insufficient storage'));
+        when(() => localDataSource.cacheFirstTimer()).thenThrow(
+          const CacheException(message: 'Insufficient storage'),
+        );
 
         final result = await repoImpl.cacheFirstTimer();
 
@@ -65,9 +60,84 @@ void main() {
             CacheFailure(message: 'Insufficient storage', statusCode: 500),
           ),
         );
-        verify(
-          () => localDataSource.cacheFirstTimer(),
-        ).called(1);
+        verify(() => localDataSource.cacheFirstTimer());
+        verifyNoMoreInteractions(localDataSource);
+      },
+    );
+  });
+
+  group('checkIfUserIsFirstTimer', () {
+    test(
+      'should return true when user is first timer',
+      () async {
+        when(
+          () => localDataSource.checkIfUserIsFirstTimer(),
+        ).thenAnswer((_) async => Future.value(true));
+
+        final result = await repoImpl.checkIfUserIsFirstTimer();
+
+        expect(
+          result,
+          equals(
+            const Right<dynamic, bool>(true),
+          ),
+        );
+
+        verify(() => localDataSource.checkIfUserIsFirstTimer()).called(1);
+
+        verifyNoMoreInteractions(localDataSource);
+      },
+    );
+
+    test(
+      'should return false when user is not first timer',
+      () async {
+        when(
+          () => localDataSource.checkIfUserIsFirstTimer(),
+        ).thenAnswer((_) async => Future.value(false));
+
+        final result = await repoImpl.checkIfUserIsFirstTimer();
+
+        expect(
+          result,
+          equals(
+            const Right<dynamic, bool>(false),
+          ),
+        );
+
+        verify(() => localDataSource.checkIfUserIsFirstTimer()).called(1);
+
+        verifyNoMoreInteractions(localDataSource);
+      },
+    );
+
+    test(
+      'should return a CacheFailure when call to local data source '
+      'is unsuccessful',
+      () async {
+        when(() => localDataSource.checkIfUserIsFirstTimer()).thenThrow(
+          const CacheException(
+            message: 'Insufficient permissions',
+            statusCode: 403,
+          ),
+        );
+
+        final result = await repoImpl.checkIfUserIsFirstTimer();
+
+        expect(
+          result,
+          equals(
+            Left<CacheFailure, bool>(
+              CacheFailure(
+                message: 'Insufficient permissions',
+                statusCode: 403,
+              ),
+            ),
+          ),
+        );
+
+        verify(() => localDataSource.checkIfUserIsFirstTimer()).called(1);
+
         verifyNoMoreInteractions(localDataSource);
       },
     );
