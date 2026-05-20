954bd068cd1e715d15357a7c10541dd0ec3fbab7
Wojciech Turek <wojciechturek9@gmail.com>
2026-05-20 19:45:33 +0200

    feat:add Auth Remote DataSource and make test pass

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:education_app/core/res/colours.dart';
 import 'package:education_app/core/res/fonts.dart';
 import 'package:education_app/core/services/injection_container.dart';
 import 'package:education_app/core/services/router.dart';
+import 'package:education_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await init();
+
   runApp(const MyApp());
 }
 
