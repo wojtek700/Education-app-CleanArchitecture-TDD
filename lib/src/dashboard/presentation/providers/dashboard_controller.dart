d344dd59e482e14ff4c82ea8556a5319ab848d57
Wojciech Turek <wojciechturek9@gmail.com>
2026-06-03 19:44:52 +0200

    feat: add Profile UI

--- lib/src/dashboard/presentation/providers/dashboard_controller.dart
+++ lib/src/dashboard/presentation/providers/dashboard_controller.dart
@@ -1,5 +1,6 @@
 import 'package:education_app/core/common/app/providers/tab_navigator.dart';
 import 'package:education_app/core/common/views/persistent_view.dart';
+import 'package:education_app/src/profile/presentation/views/profile_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -19,7 +20,7 @@ class DashboardController extends ChangeNotifier {
       child: const PersistentView(),
     ),
     ChangeNotifierProvider(
-      create: (_) => TabNavigator(TabItem(child: const Placeholder())),
+      create: (_) => TabNavigator(TabItem(child: const ProfileView())),
       child: const PersistentView(),
     ),
   ];
