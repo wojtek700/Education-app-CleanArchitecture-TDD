1f42d0917f1fb9cb8f33276dc2d38bd99f8e21b4
Wojciech Turek <wojciechturek9@gmail.com>
2026-06-09 13:35:10 +0200

    feat: add Course Repository Implementation and make a test pass

---
+++ lib/core/common/features/course/data/datasources/course_remote_data_src.dart
@@ -0,0 +1,9 @@
+import 'package:education_app/core/common/features/course/data/models/course_model.dart';
+import 'package:education_app/core/common/features/course/domain/entities/course.dart';
+
+abstract class CourseRemoteDataSrc {
+  const CourseRemoteDataSrc();
+  Future<List<CourseModel>> getCourses();
+
+  Future<void> addCourse(Course course);
+}
