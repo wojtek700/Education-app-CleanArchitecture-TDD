502fa1d2b047db8f9c466ca7b6e9c58c35882359
Wojciech Turek <wojciechturek9@gmail.com>
2026-06-11 23:48:23 +0200

    feat: make Home and Add course UI

--- lib/core/services/injection_container.main.dart
+++ lib/core/services/injection_container.main.dart
@@ -5,6 +5,27 @@ final GetIt sl = GetIt.instance;
 Future<void> init() async {
   await _initOnBoarding();
   await _initAuth();
+  await _initCourse();
+}
+
+Future<void> _initCourse() async {
+  sl
+    ..registerFactory(
+      () => CourseCubit(
+        addCourse: sl(),
+        getCourses: sl(),
+      ),
+    )
+    ..registerLazySingleton(() => AddCourse(sl()))
+    ..registerLazySingleton(() => GetCourses(sl()))
+    ..registerLazySingleton<CourseRepo>(() => CourseRepoImpl(sl()))
+    ..registerLazySingleton<CourseRemoteDataSrc>(
+      () => CourseRemoteDataSrcImpl(
+        firestore: sl(),
+        storage: sl(),
+        auth: sl(),
+      ),
+    );
 }
 
 Future<void> _initAuth() async {
