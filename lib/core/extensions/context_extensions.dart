b3c86737ad52d3e1b9fc0a8690d93aee73b76387
Wojciech Turek <wojciechturek9@gmail.com>
2026-06-12 23:48:57 +0200

    feat: add Home Header UI

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -1,6 +1,8 @@
+import 'package:education_app/core/common/app/providers/course_of_the_day_notifier.dart';
 import 'package:education_app/core/common/app/providers/tab_navigator.dart';
 import 'package:education_app/core/common/app/providers/user_provider.dart';
 import 'package:education_app/src/auth/domain/entities/user.dart';
+import 'package:education_app/src/course/domain/entities/course.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -20,6 +22,8 @@ extension ContextExt on BuildContext {
   LocalUser? get currentUser => userProvider.user;
 
   TabNavigator get tabNavigator => read<TabNavigator>();
+
+  Course? get courseOfTheDay => read<CourseOfTheDayNotifier>().courseOfTheDay;
 
   void pop() => tabNavigator.pop();
 
--- lib/src/home/presentation/refactors/home_body.dart
+++ lib/src/home/presentation/refactors/home_body.dart
@@ -3,6 +3,7 @@ import 'package:education_app/core/common/views/loading_view.dart';
 import 'package:education_app/core/common/widgets/not_found_text.dart';
 import 'package:education_app/core/utils/core_utils.dart';
 import 'package:education_app/src/course/presentation/cubit/course_cubit.dart';
+import 'package:education_app/src/home/presentation/refactors/home_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -54,7 +55,9 @@ class _HomeBodyState extends State<HomeBody> {
             );
           return ListView(
             padding: const EdgeInsets.symmetric(horizontal: 16),
-            children: [],
+            children: const [
+              HomeHeader(),
+            ],
           );
         }
         return const SizedBox.shrink();
