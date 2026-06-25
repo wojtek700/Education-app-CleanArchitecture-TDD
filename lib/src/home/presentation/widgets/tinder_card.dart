3b2771aadc7e9d14bc39c0438ad4d9a9977a9011
Wojciech Turek <wojciechturek9@gmail.com>
2026-06-25 22:34:09 +0200

    feat: add Home View and Course Detail

--- lib/src/home/presentation/widgets/tinder_card.dart
+++ lib/src/home/presentation/widgets/tinder_card.dart
@@ -25,8 +25,7 @@ class TinderCard extends StatelessWidget {
         boxShadow: [
           BoxShadow(
             color: Colors.black.withValues(alpha: 0.15),
-            offset: const Offset(0, 5),
-            blurRadius: 10,
+            offset: const Offset(0, 4),
           ),
         ],
       ),
