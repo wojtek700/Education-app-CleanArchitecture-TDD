6673f9f1a2ae539d5d0e65cf5023ea5440dafef3
Wojciech Turek <wojciechturek9@gmail.com>
2026-06-30 19:07:02 +0200

    feat: add Video player cubit and make a tests pass

--- lib/core/services/injection_container.main.dart
+++ lib/core/services/injection_container.main.dart
@@ -6,6 +6,18 @@ Future<void> init() async {
   await _initOnBoarding();
   await _initAuth();
   await _initCourse();
+  await _initVideo();
+}
+
+Future<void> _initVideo() async {
+  sl
+    ..registerFactory(() => VideoCubit(addVideo: sl(), getVideos: sl()))
+    ..registerLazySingleton(() => AddVideo(sl()))
+    ..registerLazySingleton(() => GetVideos(sl()))
+    ..registerLazySingleton<VideoRepo>(() => VideoRepoImpl(sl()))
+    ..registerLazySingleton<VideoRemoteDataSrc>(
+      () => VideoRemoteDataSrcImpl(firestore: sl(), auth: sl(), storage: sl()),
+    );
 }
 
 Future<void> _initCourse() async {
