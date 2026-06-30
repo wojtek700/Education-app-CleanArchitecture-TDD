6a12d3b99fa5f8363613fdc44c356a81cf097464
Wojciech Turek <wojciechturek9@gmail.com>
2026-06-30 22:48:10 +0200

    feat: add Materials cubit and make a test pass

--- lib/core/services/injection_container.main.dart
+++ lib/core/services/injection_container.main.dart
@@ -7,6 +7,27 @@ Future<void> init() async {
   await _initAuth();
   await _initCourse();
   await _initVideo();
+  await _initMaterial();
+}
+
+Future<void> _initMaterial() async {
+  sl
+    ..registerFactory(
+      () => MaterialCubit(
+        addMaterial: sl(),
+        getMaterials: sl(),
+      ),
+    )
+    ..registerLazySingleton(() => AddMaterial(sl()))
+    ..registerLazySingleton(() => GetMaterials(sl()))
+    ..registerLazySingleton<MaterialRepo>(() => MaterialRepoImpl(sl()))
+    ..registerLazySingleton<MaterialRemoteDataSrc>(
+      () => MaterialRemoteDataSrcImpl(
+        firestore: sl(),
+        auth: sl(),
+        storage: sl(),
+      ),
+    );
 }
 
 Future<void> _initVideo() async {
